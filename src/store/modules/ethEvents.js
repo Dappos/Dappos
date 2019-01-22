15eaf00d8b17f9067b94021d6ce10d13be9f6109
Luca Ban - Black Birdy <mesqueeb@gmail.com>
2019-01-22 18:16:46 +0900

    Add "manual scan blockchain" 👀

--- src/store/modules/ethEvents.js
+++ src/store/modules/ethEvents.js
@@ -11,6 +11,9 @@ function initialState () {
     confirmationWatcher: null,
     confirmationWatcherTxnHash: null,
     firstConfetti: false,
+    startBlock: null,
+    manuallyCheckedUntil: null,
+    erc20Contract: null,
     transactions: {
       '*': {
         from: null,
@@ -49,6 +52,10 @@ export default {
     },
     watchETHTransactions ({state, getters, rootState, rootGetters, commit, dispatch}) {
       const web3 = getters.web3
+      web3.eth.getBlockNumber()
+        .then(blockNr => {
+          dispatch('set/startBlock', blockNr)
+        })
       const posAddress = rootState.settings.wallet.address
       const subscription = web3.eth.subscribe('pendingTransactions', (error, result) => {
         if (error) {
@@ -67,6 +74,10 @@ export default {
     },
     watchErc20Transactions ({state, getters, rootState, rootGetters, commit, dispatch}, selectedToken) {
       const web3 = getters.web3
+      web3.eth.getBlockNumber()
+        .then(blockNr => {
+          dispatch('set/startBlock', blockNr)
+        })
       const posAddress = rootState.settings.wallet.address
       const tokenInfo = rootGetters['settings/selectedTokenObject']
       const selectedNetwork = rootGetters['settings/selectedNetworkObject'].name
@@ -74,28 +85,20 @@ export default {
       const erc20ContractAddress = tokenInfo.networks[selectedNetwork].address
       if (!erc20ContractAddress) throw Error('something went wrong. Erc20 address not found...')
       const erc20Contract = new web3.eth.Contract(erc20Abi, erc20ContractAddress)
+      dispatch('set/erc20Contract', erc20Contract)
       const subscription = erc20Contract.events.Transfer({fromBlock: 'latest', filter: {_to: posAddress}})
         .on('data', event => {
-          const txn = {
-            blockNumber: event.blockNumber,
-            hash: event.transactionHash,
-            from: event.returnValues.from,
-            to: event.returnValues.to,
-            value: event.returnValues.value
-          }
+          const txn = getters.parseEventIntoTxn(event)
           if (event && txn.to === posAddress) dispatch('foundTxn', txn)
-          if (txn) console.log('txn → ', (txn.to === posAddress), 'event → ', event, 'txn → ', txn)
+          // if (txn) console.log('txn → ', (txn.to === posAddress), 'event → ', event, 'txn → ', txn)
         })
       dispatch('set/subscription', subscription)
     },
     unwatchTransactions ({state, getters, rootState, rootGetters, commit, dispatch}) {
       if (!state.subscription || !state.subscription.unsubscribe) return
       state.subscription.unsubscribe((error, success) => {
-        if (success) {
-          console.log('Successfully unsubscribed!')
-        } else {
-          console.error(error)
-        }
+        if (success) return console.log('Successfully unsubscribed!')
+        console.error(error)
       })
     },
     watchConfirmations ({state, getters, rootState, rootGetters, commit, dispatch}, txnHash) {
@@ -126,6 +129,46 @@ export default {
       dispatch('unwatchConfirmations')
       commit('resetStateData')
     },
+    async manualTransactionCheck ({dispatch, state, getters}, selectedToken) {
+      const web3 = getters.web3
+      const manuallyCheckedUntil = state.manuallyCheckedUntil || state.startBlock
+      const currentBlock = await web3.eth.getBlockNumber()
+      const checkRange = {
+        from: manuallyCheckedUntil,
+        to: currentBlock
+      }
+      if (selectedToken === 'eth') return dispatch('manualETHTransactionCheck', checkRange)
+      return dispatch('manualErc20TransactionCheck', checkRange)
+    },
+    async manualETHTransactionCheck ({state, dispatch, getters, rootState}, {from, to}) {
+      const web3 = getters.web3
+      const posAddress = rootState.settings.wallet.address
+      const returnTransactionObjects = true
+      while (from <= to) {
+        web3.eth.getBlock(from, returnTransactionObjects)
+          .then(block => {
+            block.transactions.forEach(txn => {
+              if (txn && txn.to === posAddress) dispatch('foundTxn', txn)
+            })
+          })
+        from++
+      }
+      dispatch('set/manuallyCheckedUntil', from)
+    },
+    async manualErc20TransactionCheck ({state, dispatch, getters, rootState}, {from, to}) {
+      const posAddress = rootState.settings.wallet.address
+      if (!state.erc20Contract) return console.error('no erc20Contract')
+      state.erc20Contract.getPastEvents('Transfer', {
+        fromBlock: from,
+        toBlock: to
+      }).then(events => {
+        events.forEach(event => {
+          const txn = getters.parseEventIntoTxn(event)
+          if (event && txn.to === posAddress) dispatch('foundTxn', txn)
+        })
+      })
+      dispatch('set/manuallyCheckedUntil', to)
+    },
     foundTxn ({state, getters, rootState, rootGetters, commit, dispatch}, txn) {
       console.log('found TXN! → ', txn)
       dispatch('set/transactions.*', {[txn.hash]: txn})
@@ -140,7 +183,12 @@ export default {
       const txnTotalValue = getters.transactionsTotalValueConverted
       const txnValueEnough = (Number(txnTotalValue) >= Number(paymentRequest.value))
       if (txnValueEnough) {
-        dispatch('history/insert', Object.assign(paymentRequest, {txns, id: lastTransaction}), {root: true})
+        dispatch('history/insert', Object.assign(paymentRequest, {
+          txns,
+          id: lastTransaction,
+          startBlock: state.startBlock,
+          endBlock: state.transactions[lastTransaction].blockNumber,
+        }), {root: true})
         dispatch('watchConfirmations', lastTransaction)
         dispatch('modals/set/cart.payment.stage', 2, {root: true})
       }
@@ -149,7 +197,9 @@ export default {
   getters:
   {
     transactionsArray: (state, getters, rootState, rootGetters) => {
-      return Object.keys(state.transactions).filter(k => k !== '*').map(k => state.transactions[k])
+      return Object.keys(state.transactions)
+        .filter(k => k !== '*')
+        .map(k => state.transactions[k])
     },
     transactionsTotalValue: (state, getters, rootState, rootGetters) => {
       const txns = getters.transactionsArray
@@ -177,6 +227,15 @@ export default {
       const txnRef = receits[txnHash]
       if (!txnRef) return 0
       return txnRef.confirmations
-    }
+    },
+    parseEventIntoTxn: () => (event) => {
+      return {
+        blockNumber: event.blockNumber,
+        hash: event.transactionHash,
+        from: event.returnValues.from,
+        to: event.returnValues.to,
+        value: event.returnValues.value
+      }
+    },
   }
 }
